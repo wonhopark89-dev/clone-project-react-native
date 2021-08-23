e19237d2e65e5f10a3f80e6a84044006ff434862
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-08-23 23:38:13 +0900

    git remove cached

---
+++ youtube-expo/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen22</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
